98af99b5fe340ce33dd585bae66c51eb327c6610
0amr-ashraf0 <46089737+0amr-ashraf0@users.noreply.github.com>
2018-12-23 20:09:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2112,4 +2112,4 @@ client.on("message", message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login("NTI2NDU5MjM0NzkyMjQzMjAy.DwFf2w.AqPHCBLdzaS5Et3uWk0ouJK6SoM");
